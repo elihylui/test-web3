96415547aa599b485f304cfcd2ddb810df862597
elihylui <elihylui@gmail.com>
2023-10-22 11:31:29 +0100

    fix  button

--- my-app/packages/frontend/components/contract/RegisterIntent.tsx
+++ my-app/packages/frontend/components/contract/RegisterIntent.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, FormEvent } from 'react';
-import { useContractWrite, usePrepareContractWrite, useConnect, useAccount } from 'wagmi';
+import { useContractWrite, usePrepareContractWrite, useConnect, useAccount, useWaitForTransaction } from 'wagmi';
 
 import { TRADE_ABI } from '@/contracts/TRADE_ABI';
 
@@ -8,8 +8,8 @@ export const RegisterIntent = () => {
     const [amount, setAmount] = useState<number>(0); 
     const [liquidity, setLiquidity] = useState<number>(0); 
 
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('');
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState('');
 
     // const { data: signerData } = useSigner();
     const { address, isConnecting, isConnected, isDisconnected } = useAccount()
@@ -17,51 +17,63 @@ export const RegisterIntent = () => {
     const tradeAddress = '0xB05636e6d474f7eeaCdd8aF3102D9D8Df1C9e16c';
     const tradeABI = TRADE_ABI;
 
-    const {config} = usePrepareContractWrite({
+    const {config, 
+        error: prepareError,
+        isError: isPrepareError,
+    } = usePrepareContractWrite({
         address: tradeAddress,
         abi: tradeABI,
         functionName: "registerIntent",
         args: [tokenIn, amount, liquidity]
       });
     
-    const { write } = useContractWrite(config)
+    const { data, error, isError, write } = useContractWrite(config)
+ 
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
 
-    useEffect(() => {
-        if (isConnected) {
-          setError('');
-          setLoading(false);
-        } else {
-          setLoading(false);
-          setError('please connect your wallet');
-        }
-      }, [address]);
+    // useEffect(() => {
+    //     if (isConnected) {
+    //       setError('');
+    //       setLoading(false);
+    //     } else {
+    //       setLoading(false);
+    //       setError('please connect your wallet');
+    //     }
+    //   }, [address]);
 
-      const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-          setLoading(true);
-           const tx = write?.()
-        //   await tx.wait();
-          console.log(tx);
-          //setNewGreeter('');
-          setLoading(false);
-        } catch (error) {
-          setError('txn failed, check contract');
-          setLoading(false);
-        }
-      };
+    //   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     try {
+    //       setLoading(true);
+    //        const tx = await write?.()
 
-      if (loading) {
-        return <div>Loading...</div>;
-      }
+    //       console.log(tx);
+
+    //       setLoading(false);
+    //     } catch (error) {
+    //       setError('txn failed, check contract');
+    //       setLoading(false);
+    //     }
+    //   };
+
+    //   if (loading) {
+    //     return <div>Loading...</div>;
+    //   }
     
-      if (error) {
-        return <div>{error}</div>;
-      }
+    //   if (error) {
+    //     return <div>{error}</div>;
+    //   }
 
       return (
         <div style={{ margin: '20px' }}>
-          <form onSubmit={(e) => handleSubmit(e)}>
+              <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    write?.()
+                }}
+                >
             <input
               required
               value={tokenIn}
@@ -80,9 +92,20 @@ export const RegisterIntent = () => {
               placeholder="liquidity"
               onChange={(e) => setLiquidity(parseInt(e.target.value))}
             />
-            <button style={{ marginLeft: '20px' }} type="submit">
-              Register Intent
+            <button disabled={!write || isLoading}>
+                {isLoading ? 'Registering' : 'Register Intent'}
             </button>
+            {isSuccess && (
+                <div>
+                Successfully registered your intent!
+                <div>
+                    <a href={`https://sepolia.scrollscan.dev/address/${data?.hash}`}>Scrollscan</a>
+                </div>
+                </div>
+            )}
+            {(isPrepareError || isError) && (
+                <div>Error: {(prepareError || error)?.message}</div>
+            )}
           </form>
         </div>
       );   
