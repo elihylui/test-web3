0c6dbdfc6b66762407984bcd642b4bf24d490528
elihylui <elihylui@gmail.com>
2023-10-22 11:41:12 +0100

    add todo

--- my-app/packages/frontend/components/contract/RegisterIntent.tsx
+++ my-app/packages/frontend/components/contract/RegisterIntent.tsx
@@ -8,14 +8,9 @@ export const RegisterIntent = () => {
     const [amount, setAmount] = useState<number>(0); 
     const [liquidity, setLiquidity] = useState<number>(0); 
 
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState('');
-
-    // const { data: signerData } = useSigner();
-    const { address, isConnecting, isConnected, isDisconnected } = useAccount()
-
-    const tradeAddress = '0xB05636e6d474f7eeaCdd8aF3102D9D8Df1C9e16c';
+    const tradeAddress = '0xB05636e6d474f7eeaCdd8aF3102D9D8Df1C9e16c'; //change this to new address
     const tradeABI = TRADE_ABI;
+    const WETH_ADDRESS = ''; //TODO: add WETH address
 
     const {config, 
         error: prepareError,
@@ -33,39 +28,6 @@ export const RegisterIntent = () => {
         hash: data?.hash,
     })
 
-    // useEffect(() => {
-    //     if (isConnected) {
-    //       setError('');
-    //       setLoading(false);
-    //     } else {
-    //       setLoading(false);
-    //       setError('please connect your wallet');
-    //     }
-    //   }, [address]);
-
-    //   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    //     e.preventDefault();
-    //     try {
-    //       setLoading(true);
-    //        const tx = await write?.()
-
-    //       console.log(tx);
-
-    //       setLoading(false);
-    //     } catch (error) {
-    //       setError('txn failed, check contract');
-    //       setLoading(false);
-    //     }
-    //   };
-
-    //   if (loading) {
-    //     return <div>Loading...</div>;
-    //   }
-    
-    //   if (error) {
-    //     return <div>{error}</div>;
-    //   }
-
       return (
         <div style={{ margin: '20px' }}>
               <form
@@ -78,7 +40,7 @@ export const RegisterIntent = () => {
               required
               value={tokenIn}
               placeholder="tokenIn"
-              onChange={(e) => setTokenIn(e.target.value)}
+              onChange={(e) => setTokenIn(e.target.value)} //default to WETH
             />
             <input
               required
